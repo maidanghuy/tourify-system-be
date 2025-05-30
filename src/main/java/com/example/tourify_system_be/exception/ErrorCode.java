a1d92177a23e3e35f3c007ea5c685bfb524a99ab
maidanghuy <maidanghuy31@gmail.com>
2025-05-30 14:53:46 +0700

    Add API response handling, user mapping, and exception handling modules
    - Added `APIResponse` class for standardized API responses.
    - Introduced `UserMapper` for mapping between entities and DTOs.
    - Implemented exception handling mechanism with custom `ErrorCode` and `AppException`.
    - Updated `UserController` and `UserService` to utilize the new response and mapping structure.
    - Enhanced `GlobalExceptionHandler` for detailed error management.

---
+++ src/main/java/com/example/tourify_system_be/exception/ErrorCode.java
@@ -0,0 +1,26 @@
+package com.example.tourify_system_be.exception;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+@Getter
+public enum ErrorCode {
+    UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR),
+    INVALID_KEY(1001, "Uncategorized error", HttpStatus.BAD_REQUEST),
+    USER_EXISTED(1002, "User existed", HttpStatus.BAD_REQUEST),
+    USER_NOT_FOUND(1001, "User not found", HttpStatus.NOT_FOUND),
+    ;
+
+    ErrorCode(int code, String message, HttpStatusCode statusCode) {
+        this.code = code;
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+
+    private final int code;
+    private final String message;
+    private final HttpStatusCode statusCode;
+
+}
--- src/main/java/com/example/tourify_system_be/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/tourify_system_be/exception/GlobalExceptionHandler.java
@@ -1,16 +1,75 @@
 package com.example.tourify_system_be.exception;
 
+import com.example.tourify_system_be.dto.request.APIResponse;
+import jakarta.validation.ConstraintViolation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Map;
+import java.util.Objects;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final String MIN_ATTRIBUTE = "min";
+
     @ExceptionHandler(value = Exception.class)
-    void handleRuntimeException(RuntimeException e){
+    ResponseEntity<APIResponse> handleRuntimeException(RuntimeException e){
+        APIResponse response = new APIResponse();
+        response.setCode(ErrorCode.UNCATEGORIZED_EXCEPTION.getCode());
+        response.setMessage(e.getMessage());
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(value = AppException.class)
+    ResponseEntity<APIResponse> handleAppException(AppException e){
+        ErrorCode errorCode = e.getErrorCode();
+
+
+        APIResponse response = new APIResponse();
+        response.setCode(errorCode.getCode());
+        response.setMessage(e.getMessage());
+
+        return ResponseEntity.status(errorCode.getStatusCode()).body(response);
     }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    void handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+    ResponseEntity<APIResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        String enumKey = e.getFieldError().getDefaultMessage();
+
+        ErrorCode errorCode = ErrorCode.INVALID_KEY;
+        Map<String, Object> attributes = null;
+        try {
+            errorCode = ErrorCode.valueOf(enumKey);
+
+            var constraintViolation =
+                    e.getBindingResult().getAllErrors().getFirst().unwrap(ConstraintViolation.class);
+
+            attributes = constraintViolation.getConstraintDescriptor().getAttributes();
+
+//            log.info(attributes.toString());
+
+        } catch (IllegalArgumentException ie) {
+
+        }
+
+        APIResponse apiResponse = new APIResponse();
+
+        apiResponse.setCode(errorCode.getCode());
+        apiResponse.setMessage(
+                Objects.nonNull(attributes)
+                        ? mapAttribute(errorCode.getMessage(), attributes)
+                        : errorCode.getMessage());
+
+        return ResponseEntity.badRequest().body(apiResponse);
+    }
+
+    private String mapAttribute(String message, Map<String, Object> attributes) {
+        String minValue = String.valueOf(attributes.get(MIN_ATTRIBUTE));
+
+        return message.replace("{" + MIN_ATTRIBUTE + "}", minValue);
     }
 }
