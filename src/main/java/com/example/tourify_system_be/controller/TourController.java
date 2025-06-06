a3ccfe7f21aa1187ec8a825e03e47b718df24f03
vu12082004 <vunhde180288@fpt.edu.vn>
2025-06-06 13:55:18 +0700

    search1
    * register
    
    * register2
    
    * register3
    
    * fix URL routing in reset password functionality and forgot password page
    
    * fix URL routing in reset password functionality and forgot password page
    
    * search commit1
    
    ---------
    
    Co-authored-by: NGUYENHUYVU-DE180288 <vunhde18088@fpt.edu.vn>

---
+++ src/main/java/com/example/tourify_system_be/controller/TourController.java
@@ -0,0 +1,23 @@
+package com.example.tourify_system_be.controller;
+
+import com.example.tourify_system_be.dto.request.TourSearchRequest;
+import com.example.tourify_system_be.dto.response.TourResponse;
+import com.example.tourify_system_be.entity.Tour;
+import com.example.tourify_system_be.service.TourService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tours")
+@RequiredArgsConstructor
+public class TourController {
+
+    private final TourService tourService;
+
+    @PostMapping("/search")
+    public List<TourResponse> searchTours(@RequestBody TourSearchRequest request) {
+        return tourService.searchTours(request);
+    }
+}
