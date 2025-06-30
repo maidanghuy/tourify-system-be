720bbf1c1723098636d13372b68682abf2d3c6b6
tranhongphong <tranhongphong577@gmail.com>
2025-07-01 02:32:14 +0700

    Refactor booking feature: modularize JavaScript into `booking.js`, enhance dynamic tour display and price calculation, adjust breadcrumb placeholders, update `TourMapper` with `UserResponse` mapping logic, and rename `BookingTourRepository` to `IBookingTourRepository` for consistency.

--- src/main/java/com/example/tourify_system_be/mapper/TourMapper.java
+++ src/main/java/com/example/tourify_system_be/mapper/TourMapper.java
@@ -2,7 +2,9 @@ package com.example.tourify_system_be.mapper;
 
 import com.example.tourify_system_be.dto.request.TourCreateRequest;
 import com.example.tourify_system_be.dto.response.TourResponse;
+import com.example.tourify_system_be.dto.response.UserResponse;
 import com.example.tourify_system_be.entity.Tour;
+import com.example.tourify_system_be.entity.User;
 import org.mapstruct.*;
 import java.math.BigDecimal;
 
@@ -12,6 +14,7 @@ public interface TourMapper {
     @Mapping(target = "createdByUserName", source = "manageBy.userName")
     @Mapping(target = "placeName", source = "place.placeName")
     @Mapping(target = "categoryName", source = "category.categoryName")
+    @Mapping(target = "managementBy", source = "manageBy") // ánh xạ User → UserRespons
     TourResponse toResponse(Tour tour);
 
     @Mapping(target = "manageBy", ignore = true)
@@ -19,9 +22,8 @@ public interface TourMapper {
     @Mapping(target = "category", ignore = true)
     Tour toEntity(TourCreateRequest request);
 
-    @Mapping(target = "createdByUserName", source = "manageBy.userName")
-    @Mapping(target = "placeName", source = "place.placeName")
-    @Mapping(target = "categoryName", source = "category.categoryName")
-    TourResponse toResponse1(Tour tour);
 
+    // MapStruct sẽ gọi mapper này để ánh xạ User → UserResponse
+    UserResponse map(User user);
+    TourResponse toResponse1(Tour tour);
 }
