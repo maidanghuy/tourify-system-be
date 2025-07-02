dfe92f0e333f12125b94d07ffd54270ad5f65c30
NGUYENHUYVU-DE180288 <vunhde18088@fpt.edu.vn>
2025-07-02 17:20:01 +0700

    Enhance: consolidate category filtering with dynamic "active" category retrieval, streamline search form state preservation across views, and implement `CategoryService` for reusable functionality.

--- src/main/java/com/example/tourify_system_be/repository/ICategoryRepository.java
+++ src/main/java/com/example/tourify_system_be/repository/ICategoryRepository.java
@@ -3,5 +3,9 @@ package com.example.tourify_system_be.repository;
 import com.example.tourify_system_be.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ICategoryRepository extends JpaRepository<Category,String> {
+import java.util.List;
+
+public interface ICategoryRepository extends JpaRepository<Category, String> {
+    List<Category> findByStatusIgnoreCase(String status);
 }
+
